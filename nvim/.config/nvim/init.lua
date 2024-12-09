965907ebf69ce31d9184b0343267410e71487d0f
Samúel Jón Gunnarsson <samuel.jon.gunnarsson@gmail.com>
2024-12-09 21:26:20 +0100

    making the editor background transparent

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("neogit")
+
+-- Make the theme background transparent
+vim.o.background = "dark"
+vim.cmd("hi Normal ctermbg=none guibg=none")
+vim.cmd("hi VertSplit ctermbg=none guibg=none")
+vim.cmd("hi StatusLine ctermbg=none guibg=none")
+vim.cmd("hi LineNr ctermbg=none guibg=none")
+vim.cmd("hi CursorLineNr ctermbg=none guibg=none")
+vim.cmd("hi Pmenu ctermbg=none guibg=none")
+vim.cmd("hi PmenuSel ctermbg=none guibg=none")
